aa742a715f87d7c5395b5aa2f7a7128ebda7193f
Camilo Rojas <camilo.rojas@outlook.jp>
2024-06-20 21:45:12 -0400

    style: run format and lint

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -2,25 +2,24 @@
 import { onMounted, ref } from 'vue';
 
 const model = defineModel({
-    type: String,
-    required: true,
+  type: String,
+  required: true,
 });
 
 const input = ref(null);
 
 onMounted(() => {
-    if (input.value.hasAttribute('autofocus')) {
-        input.value.focus();
-    }
+  if (input.value.hasAttribute('autofocus')) {
+    input.value.focus();
+  }
 });
 
 defineExpose({ focus: () => input.value.focus() });
 </script>
 
 <template>
-    <input
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-        v-model="model"
-        ref="input"
-    />
+  <input
+    ref="input"
+    v-model="model"
+    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" />
 </template>
